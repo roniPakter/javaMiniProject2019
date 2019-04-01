13e5290d53c470a6026d3b3748148740c70376d6
ronip <ronip@DESKTOP-IMFGCTT>
2019-04-01 22:10:32 +0300

    טסטים של חיתוכי ספירה

--- exe1/src/tests/SphereTest.java
+++ exe1/src/tests/SphereTest.java
@@ -20,7 +20,7 @@ import primitives.Vector;
  */
 public class SphereTest {
 	Sphere sphere = new Sphere(1, new Point(0, 0, 1));
-	Sphere sphere2 = new Sphere(2,  new Point(0,0,0));
+	Sphere sphere2 = new Sphere(2,  Point.ZERO);
 
 	/**
 	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
@@ -35,16 +35,120 @@ public class SphereTest {
 	 */
 	@Test
 	public void testFindIntersections() {
-		//a list with all expected
-		 List<Point> insecPoints = new ArrayList<Point>() {{
-			 add(new Point(0,-1.93202,0.51699));
-			 add(new Point(0,1.46144,1.36536));
-		 }};
-		Ray ray = new Ray(new Point(0,-4,0), new Vector(0,4,1));
-		List<Point> list  = sphere2.findIntersections(ray);
-		assertTrue(list.size() == 2);
-		assertTrue( list.containsAll(insecPoints));
+		Vector directionVector = new Vector(0,4,1);
+		//expected intersections
+		Point insec1 = new Point(0,-1.93202,0.51699);
+		Point insec2 = new Point(0,1.46144,1.36536);
+		 //a ray intersects the sphere twice
+		Point point = new Point(0,-4,0);
+		Ray ray = new Ray(point, directionVector);
+		List<Point> actualInsecPoints  = sphere2.findIntersections(ray);
 		
+		/**
+		 * Check a regular two-intersections case
+		 */
+		assertTrue(actualInsecPoints.size() == 2 && actualInsecPoints.contains(insec1) && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * Check the opposite ray - no intersections
+		 */
+		Ray oppositeRay = new Ray(new Point(0,-4,0), directionVector.scale(-1));
+		assertTrue(sphere2.findIntersections(oppositeRay).isEmpty());
+		
+		/**
+		 * Check one-intersection case a ray from within the sphere
+		 */
+		Ray fromWithinRay = new Ray(new Point(0,0,1), directionVector);
+		actualInsecPoints = sphere2.findIntersections(fromWithinRay);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * Check no-intersection case - the ray in not in the direction of the sphere at all
+		 */
+		Ray otherDirectionRay = new Ray(new Point(0,0,3), new Vector(0,5,-3));
+		assertTrue(sphere2.findIntersections(otherDirectionRay).isEmpty());
+		
+		/**
+		 * Check the case the ray starts on the sphere and goes through it
+		 */
+		Ray goesThroughRay = new Ray(insec1, directionVector);
+		actualInsecPoints = sphere2.findIntersections(goesThroughRay);
+		assertTrue(actualInsecPoints.size() == 2 && actualInsecPoints.contains(insec1) && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * Check the case the ray starts on the sphere and goes outwards
+		 */
+		Ray goesOutwardsRay = new Ray(insec2, directionVector);
+		actualInsecPoints = sphere2.findIntersections(goesOutwardsRay);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(insec2));
+		
+		//Check the case the ray goes through the center point
+		directionVector = new Vector(0,1,0);
+		Ray throughCenteRay = new Ray(point, directionVector);
+		insec1 = new Point(0,-2,0);
+		insec2 = new Point(0,2,0);
+		actualInsecPoints = sphere2.findIntersections(throughCenteRay);
+		/**
+		 * the ray starts before the sphere and goes through the center point
+		 */
+		assertTrue(actualInsecPoints.size() == 2 && actualInsecPoints.contains(insec1) && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * the ray goes through the center point and starts in the sphere
+		 */
+		throughCenteRay = new Ray(new Point(0,0.5,0), directionVector);
+		actualInsecPoints = sphere2.findIntersections(throughCenteRay);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * the ray goes through the center point and starts from the center
+		 */
+		throughCenteRay = new Ray(Point.ZERO, directionVector);
+		actualInsecPoints = sphere2.findIntersections(throughCenteRay);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * the ray start on the sphere outwards ant is in the direction of the center
+		 */
+		throughCenteRay = new Ray(insec2, directionVector);
+		actualInsecPoints = sphere2.findIntersections(throughCenteRay);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * the ray starts on the sphere inwards and goes through the center
+		 */
+		throughCenteRay = new Ray(insec1, directionVector);
+		actualInsecPoints = sphere2.findIntersections(throughCenteRay);
+		assertTrue(actualInsecPoints.size() == 2 && actualInsecPoints.contains(insec1) && actualInsecPoints.contains(insec2));
+		
+		/**
+		 * the ray doesn't touch the sphere but starts orthogonal to the center point
+		 */
+		ray = new Ray(point ,new Vector(0,0,1));
+		actualInsecPoints = sphere2.findIntersections(throughCenteRay);
+		assertTrue(sphere2.findIntersections(ray).isEmpty());
+		
+		/**
+		 * the ray osculates the sphere and starts before it
+		 */
+		ray = new Ray(new Point(0,-4,2), directionVector);
+		Point topPoint = new Point(0,0,2);
+		actualInsecPoints = sphere2.findIntersections(ray);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(topPoint));
+		
+		/**
+		 * the ray osculates the sphere and starts within it
+		 */
+		ray = new Ray(topPoint, directionVector);
+		actualInsecPoints = sphere2.findIntersections(ray);
+		assertTrue(actualInsecPoints.size() == 1 && actualInsecPoints.contains(topPoint));
+		
+		/**
+		 * the ray could osculate the sphere but starts after it
+		 */
+		ray = new Ray(new Point(0,1,2), directionVector);
+		actualInsecPoints = sphere2.findIntersections(ray);
+		assertTrue(actualInsecPoints.isEmpty());
 	}
 
 }
