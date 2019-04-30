02327217b3287ee924949944850dd30d21898a62
ronip <ronip@DESKTOP-IMFGCTT>
2019-04-30 16:43:32 +0300

    תרגיל 4 גמור

--- exe1/src/tests/CameraTest.java
+++ exe1/src/tests/CameraTest.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 
 import elements.Camera;
 import geometries.Intersectable;
+import geometries.Plane;
 import geometries.Sphere;
+import geometries.Triangle;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
@@ -51,11 +53,31 @@ public class CameraTest {
 	@Test
 	public void testCameraRaysIntersection() {
 		// tests for a Geometric shapes
-		Camera camera = new Camera(Point.ZERO, negativeZAxis, Vector.NEGATIVE_Y_AXIS);
+		Camera camera = new Camera(new Point(0,0,0.5), negativeZAxis, Vector.NEGATIVE_Y_AXIS);
 		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Sphere(1, new Point(0, 0, -3))) == 2);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Sphere(2.5, new Point(0, 0, -2.5))) == 18);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Sphere(2, new Point(0, 0, -2))) == 10);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Sphere(10, new Point(0, 0, -2))) == 9);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Sphere(0.5, new Point(0, 0, 3))) == 0);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Plane(new Point(0,0,-2), Vector.Z_AXIS)) == 9);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Triangle(new Point(1,1,-2),new Point(-1,1,-2),new Point(0,-1,-2))) == 1);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3, new Triangle(new Point(1,1,-2),new Point(-1,1,-2),new Point(0,-20,-2))) == 2);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3,new Plane(new Point(0,-1,-1.75),new Vector(0,-1,0.5))) == 6);
+		assertTrue(countAllIntersectionsFromCamera(camera, 3, 3, 1, 3, 3,new Plane(new Point(0,0,-0.5),new Vector(0,-5,10))) == 9);
 
 	}
 
+	/**
+	 * counts the intersection points of all rays from given camera and view-plane with a given intersectable shape 
+	 * @param camera
+	 * @param nx the number of rows
+	 * @param ny the number of columns
+	 * @param screenDistance distance of the view-plane from the camera
+	 * @param screenWidth 
+	 * @param screenHeight
+	 * @param shape
+	 * @return
+	 */
 	private int countAllIntersectionsFromCamera(Camera camera, int nx, int ny, double screenDistance, double screenWidth, double screenHeight, Intersectable shape) {
 		ArrayList<Point> intersectionList = new ArrayList<Point>();
 		for (int i = 0; i < nx; ++i) {
@@ -66,18 +88,4 @@ public class CameraTest {
 		}
 		return intersectionList.size();
 	}
-
-	/*
-	 * @Test public void testCameraRaysIntersection() { ArrayList<Point>
-	 * intersectionList = new ArrayList<Point>(); Sphere sphere = new Sphere(1, new
-	 * Point(0,0,-3));
-	 * 
-	 * for (int i = 0; i < 3; ++i) { for(int j = 0; j < 3; ++j) { Ray ray =
-	 * camera.constructRayThroughPixel(3, 3, i, j, 1, 3, 3);
-	 * intersectionList.addAll(sphere.findIntersections(ray)); } } }
-	 * 
-	 * private ArrayList<Point> findAllIntersectionsFromCamera(Camera camera , int
-	 * nx, int ny, double screenDistance, double screenWidth, double screenHeight,
-	 * Intersectable shape){ return null; }
-	 */
 }
