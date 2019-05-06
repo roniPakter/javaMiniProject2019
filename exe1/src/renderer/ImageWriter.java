57962a5e1bc115f554da6ab9f7e9b8da5fb5e810
ronip <ronip@DESKTOP-IMFGCTT>
2019-05-06 19:48:13 +0300

    public try

--- exe1/src/renderer/ImageWriter.java
+++ exe1/src/renderer/ImageWriter.java
@@ -11,7 +11,7 @@ import javax.imageio.stream.*;
 public class ImageWriter {
 //go22gogogogogogo33
 	private double _imageWidth, _imageHeight;
-	private int _nX, _nY;
+	public int _nX, _nY;
 
 	final String PROJECT_PATH = System.getProperty("user.dir");
 
