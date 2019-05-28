38a2d073a1984d9869f6d0ea8bbd23879c292f35
ronip <ronip@DESKTOP-IMFGCTT>
2019-05-28 09:00:10 +0300

    יום שלישי בבוקר 28.5

--- exe1/src/geometries/Geometry.java
+++ exe1/src/geometries/Geometry.java
@@ -1,31 +1,39 @@
 package geometries;
 
-import java.lang.annotation.Retention;
-
 import primitives.*;
 /**
  * Abstract class for every geometry shape
  */
 public abstract class Geometry implements Intersectable{
 	
-	/**
-	 * emission light of each geometric body
-	 */
+	/**emission light of each geometric body*/
 	protected Color _emission;
+	/**the material the of the geometric body*/
 	protected Material _material;
-	// ***************** Getters/Setters ******************** //
+	
+	// ***************** Getters ******************** //
 	/**
 	 * get the emission light of the geometric body
 	 */
 	public Color getEmission() {
 		return _emission;		
 	}
+	
+	/**
+	 * get the material the geometric body is made of
+	 */
 	public Material getMaterial() {
 		return _material;
 	}	
+	
+	// ***************** Setters ******************** //
+	/**
+	 * set the material the geometric body is made of
+	 */
 	public void setMaterial(Material material) {
 		_material = material;
 	}
+	
 	/**
 	 * set the emission light of the geometric body
 	 */
